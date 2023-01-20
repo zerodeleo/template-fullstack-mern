c8ed36524bb20611d6b1f9c954eaed365b3cc264
zerodeleo <julia.stjerna@hotmail.se>
2023-01-20 23:47:20 +0100

    test (deployment): add basename router

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,19 +1,20 @@
 import React, { FC } from 'react';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Users from './pages/users/Users';
 import User from './pages/users/[username]';
+import { CLIENT_URL } from './service';
 
 const Router: FC = () => {
   return (
-    <HashRouter>
+    <BrowserRouter basename={CLIENT_URL}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/users" element={<Users />} />
         <Route path="/users/:username" element={<User />} />
         <Route path="*" element={<p>404 Page not found</p>} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
--- client/src/service/index.ts
+++ client/src/service/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const API_URL: string = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : 'http://localhost:8080';
+export const CLIENT_URL: string = import.meta.env.VITE_CLIENT_URL ? import.meta.env.VITE_CLIENT_URL : '';
 
 export const ping = async () => {
   return await axios.get(`${API_URL}/ping`);
