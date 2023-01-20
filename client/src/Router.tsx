e13f2dbdcc1a00e298ae0d1d7a92a19e945b67c0
zerodeleo <julia.stjerna@hotmail.se>
2023-01-20 23:27:37 +0100

    test (deployment): add HashRouter

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,19 +1,19 @@
 import React, { FC } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Users from './pages/users/Users';
 import User from './pages/users/[username]';
 
 const Router: FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/users" element={<Users />} />
         <Route path="/users/:username" element={<User />} />
         <Route path="*" element={<p>404 Page not found</p>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
